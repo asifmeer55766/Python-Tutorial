a720244b3c82c84612353f1794666bf69f3593e7
Asif Hussain <158553805+asifmeer55766@users.noreply.github.com>
2024-11-01 08:12:46 +0500

    First commit

---
+++ Class01-FirstPrograme.py
@@ -0,0 +1,12 @@
+print("my name is Asif Hussain");
+
+print(23230);
+print("hello world ","this is my first program");
+print("Ans is :",45+20);
+
+
+name = "asif"
+roll_no = 23230
+age = 23
+
+print("my name is : ",name," Roll no :",roll_no," Age",age);
